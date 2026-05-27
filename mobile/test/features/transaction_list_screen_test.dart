25c0deedbac45be9dbe3b0f08fcf68163660ee12
mohammad khaufillah <khaufillahmohammad@gmail.com>
2026-05-27 19:04:52 +0000

    fix: test mock constructor mismatch for TransferOwner
    _MockTxNotifier expects 2 args after constructor change

--- mobile/test/features/transaction_list_screen_test.dart
+++ mobile/test/features/transaction_list_screen_test.dart
@@ -11,7 +11,7 @@ import '../helpers/mocks.dart';
 /// Mock notifier that does NOT auto-load on init (screen's initState will call
 /// load(), but we override it to be a no-op since we control state directly).
 class _MockTxNotifier extends TransactionListNotifier {
-  _MockTxNotifier() : super(TransactionRepository(MockApiClient()));
+  _MockTxNotifier() : super(TransactionRepository(MockApiClient()), MockApiClient());
 
   @override
   Future<void> load({bool refresh = false}) async {
