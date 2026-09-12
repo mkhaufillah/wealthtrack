73e2e07f69fa03d999f6a9ea5b2aabf87708e112
Task Subagent <vps@wealthtrack.test>
2026-09-12 15:26:18 +0700

    fix(mobile): drop now-unused kpr_model import in kpr_provider_test

--- mobile/test/features/debt/providers/kpr_provider_test.dart
+++ mobile/test/features/debt/providers/kpr_provider_test.dart
@@ -1,7 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wealthtrack/features/debt/kpr/providers/kpr_provider.dart';
-import 'package:wealthtrack/features/debt/models/kpr_model.dart';
 import 'package:wealthtrack/shared/providers/app_providers.dart';
 import '../../../helpers/mocks.dart';
 
